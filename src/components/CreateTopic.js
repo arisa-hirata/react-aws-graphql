af6b9b02c162e7640fff6075d7eb0bfdd087dc23
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-06-10 15:45:57 -0700

    make forms work on LogIn and CreateTopic components

--- src/components/CreateTopic.js
+++ src/components/CreateTopic.js
@@ -1,23 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OneColumn from './OneColumn';
 
 const CreateTopic = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const onInputChange = e => {
+    if (e.target.id === 'title') {
+      setTitle(e.target.value);
+    } else if (e.target.id === 'description') {
+      setDescription(e.target.value);
+    } else if (e.target.id === 'tag') {
+      setTags(e.target.value);
+    }
+  };
+
+  const createTopic = () => {
+    let tagArray = tags
+      .split(',')
+      .map(tag => tag.trim(' '))
+      .filter(tag => tag.length);
+    let topic = {
+      title,
+      description,
+      tags: tagArray
+    };
+    // access to dynamoDB
+  };
+
   return (
     <OneColumn title="Ask Question">
       <div className="ui form textarea">
         <div className="field">
           <label>Title</label>
-          <input placeholder="Type title here..." />
+          <input
+            id="title"
+            placeholder="Type title here..."
+            value={title}
+            onChange={onInputChange}
+          />
         </div>
         <div className="field">
           <label>Description</label>
-          <textarea placeholder="Write your description here..." />
+          <textarea
+            id="description"
+            placeholder="Write your description here..."
+            value={description}
+            onChange={onInputChange}
+          />
         </div>
         <div className="field">
           <label>Tag</label>
-          <textarea placeholder="Add tags..." />
+          <textarea
+            id="tag"
+            placeholder="Ex. dog, food, birthday"
+            value={tags}
+            onChange={onInputChange}
+          />
         </div>
-        <button className="positive btn ui button center">Create</button>
+        <button onClick={createTopic} className="positive btn ui button center">
+          Create
+        </button>
       </div>
     </OneColumn>
   );
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import OneColumn from './OneColumn';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import OneColumn from './OneColumn';
 
 const LogIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onInputChange = e => {
+    if (e.target.id === 'email') {
+      setEmail(e.target.value);
+    } else if (e.target.id === 'password') {
+      setPassword(e.target.value);
+    }
+  };
+
+  const logIn = () => {
+    // access to dynamoDB
+  };
+
   return (
     <OneColumn title="Log In">
       <div className="log-in input-form">
@@ -10,17 +25,31 @@ const LogIn = () => {
           <label htmlFor="email" className="ui label">
             <i className="mail icon" />
           </label>
-          <input type="text" placeholder="Email" id="email" />
+          <input
+            type="text"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onInputChange}
+          />
         </div>
         <br />
         <div className="ui labeled input">
           <label htmlFor="password" className="ui label">
             <i className="key icon" />
           </label>
-          <input type="password" placeholder="Password" id="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={onInputChange}
+          />
         </div>
         <br />
-        <button className="btn">Log In</button>
+        <button onClick={logIn} className="btn">
+          Log In
+        </button>
         <div className="to-signup">
           You don't have account?
           <Link to="/signup">
